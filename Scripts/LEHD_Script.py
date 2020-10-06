0a5d6eba88aeb8cd6d84d45a6b843fa3cafbd765
joeamoroso <joe.amoroso@rsginc.com>
2020-10-06 19:17:38 -0400

    I am a God agmonst men.

--- Scripts/LEHD_Script.py
+++ Scripts/LEHD_Script.py
@@ -10,7 +10,13 @@ import numpy as np
 import tarfile
 import wget
 import requests
+import gzip
+import shutil
 
+
+# =============================================================================
+# Gather US Census LEHD Data from HTTS links
+# =============================================================================
 states = ['dc','md','va']
 types = ['JT00','JT03','JT04']
 years = ['2011','2012','2013','2014','2015','2016','2017']
@@ -19,6 +25,7 @@ files = []
 
 os.chdir('C:\\CS-5010-Group-Project\\Data\\')
 
+# Loop through years 2011-2017 for DC, MD, VA and extract total, federal, and private employment tarfiles
 for year in years:
 
     for state in states:
@@ -32,13 +39,12 @@ for year in years:
            
             # with tarfile.open(url,mode ='r') as f:
             #      f.extract(r'C:/CS-5010-Group_Project/' + state)
-    dfs = []        
+
+# Loop through each tarfile and extract to CSV
 for file in os.listdir():
-    df = pd.read_csv(file,compression ='gzip')
-    dfs.append(df)
-     
-
-
-
-
+    if file.endswith('.gz'):
+        file_s = file[:-3]
+        with gzip.open(file,'rb') as f_in:
+            with gzip.open(file_s,'wb') as f_out:
+                shutil.copyfileobj(f_in,f_out)
 
