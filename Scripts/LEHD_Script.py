665a26430523656cfeccf8f01615619860b4a11c
joeamoroso <joe.amoroso@rsginc.com>
2020-10-26 19:12:17 -0400

    Add Total Employment field

--- Scripts/LEHD_Script.py
+++ Scripts/LEHD_Script.py
@@ -14,6 +14,8 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from descartes import PolygonPatch
 from shapely.geometry import Point, LineString, Polygon, shape, GeometryCollection
+import json
+import plotly.express as px
 
 # =============================================================================
 # Gather US Census LEHD Data from HTTS links
@@ -65,7 +67,7 @@ for file in os.listdir():
 df = pd.concat(data_list)
 
 # =============================================================================
-# Data Queries
+# Clean Data 
 # =============================================================================
 
 # Renaming Employment Types # 
@@ -73,7 +75,8 @@ df['Emp_Type'] = np.where(df['Emp_Type'] == 'JT00','All',df['Emp_Type'])
 df['Emp_Type'] = np.where(df['Emp_Type'] == 'JT03','Private',df['Emp_Type'])
 df['Emp_Type'] = np.where(df['Emp_Type'] == 'JT04','Federal',df['Emp_Type'])
 
-df.rename(columns = {'CNS01': 'NAICS_11',
+df.rename(columns = {'C000': 'Tot_Emp',
+                     'CNS01': 'NAICS_11',
                      'CNS02': 'NAICS_21',
                      'CNS03': 'NAICS_22',
                      'CNS04': 'NAICS_23',
@@ -100,18 +103,38 @@ df['GEOID'] = df['GEOID'].astype('int64')
 
 # Aggregate Data by Tract, Year, and Employment Group 
 
-df_tract = df[['GEOID','Year','Emp_Type','NAICS_11','NAICS_21','NAICS_22','NAICS_23',
+df_tract = df[['GEOID','Year','Emp_Type','Tot_Emp','NAICS_11','NAICS_21','NAICS_22','NAICS_23',
                'NAICS_31_33','NAICS_42','NAICS_44_45','NAICS_48_49',
                'NAICS_51','NAICS_52','NAICS_53','NAICS_54','NAICS_55','NAICS_56',
                'NAICS_61','NAICS_62','NAICS_71','NAICS_72','NAICS_81','NAICS_92']].groupby(['GEOID','Year','Emp_Type']).agg('sum').reset_index()
 
-df_tract.to_csv('LEHD_Tract.csv')
+df_tract.to_csv('LEHD_Tract.csv',index = False)
 
 # =============================================================================
 #  Import DMV Shapefile and join Data 
 # =============================================================================
-dmv = gpd.read_file('C:/CS-5010-Group-Project/Shapefile/DC_MD_VA_Tracts.shp')
-dmv.plot(facecolor = 'none',edgecolor ='black', lw =0.4)
+#dmv = gpd.read_file('C:/CS-5010-Group-Project/Shapefile/DC_MD_VA_Tracts.shp')
+#dmv.plot(facecolor = 'none',edgecolor ='black', lw =0.4)
+
+
+# =============================================================================
+# Queries
+# =============================================================================
+# with open('geojson/DC_Metro_Area_WGS.geojson') as f:
+#   dc_tracts = json.load(f)
+
+# dat = pd.read_csv('LEHD_Tract.csv')
+
+# max_value = dat['NAICS_62'].max()
+# fig = px.choropleth(dat, geojson=dc_tracts, locations='GEOID',       
+#                            color='NAICS_21',
+#                            color_continuous_scale="Viridis",
+#                            range_color=(0, max_value),
+#                            featureidkey="properties.GEOID",
+#                            projection="mercator"
+#                           )
+# fig.update_geos(fitbounds="locations", visible=False)
+# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 
 
@@ -119,5 +142,5 @@ dmv.plot(facecolor = 'none',edgecolor ='black', lw =0.4)
 
 
 
-#df_tract.to_csv('all_LEHD_2011-2015_tract.csv')
+
 
