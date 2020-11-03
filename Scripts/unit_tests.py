5c1aed0cd891d167c7f685504bf3d7582c68bd4c
joeamoroso <joe.amoroso@rsginc.com>
2020-11-02 20:32:19 -0500

    add unit tests

---
+++ Scripts/unit_tests.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 18:09:56 2020
+
+@author: Joe
+"""
+import unittest
+import os
+from LEHD_Script import *
+
+
+
+# =============================================================================
+# Check if all 47 files are read in 
+# =============================================================================
+
+
+files = [f for f in os.listdir() if f.endswith('.csv')]
+
+
+
+
+
+
+
