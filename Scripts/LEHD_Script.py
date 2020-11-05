fa32a13bc40b98998be26e94fac2a817c7625b34
joeamoroso <joe.amoroso@rsginc.com>
2020-11-05 18:18:19 -0500

    Add unit tests and try except in web scaper

--- Scripts/LEHD_Script.py
+++ Scripts/LEHD_Script.py
@@ -27,43 +27,49 @@ years = ['2011','2012','2013','2014','2015']
 os.chdir('C:\\CS-5010-Group-Project\\Data\\')
 
 # Loop through years 2011-2017 for DC, MD, VA and extract total, federal, and private employment tarfiles
-for year in years:
+try:
 
-    for state in states:
+    for year in years:
         
-        for jobtype in types:
-            url = 'https://lehd.ces.census.gov/data/lodes/LODES7/'+ state +'/wac/'+ state + '_wac_S000_' + jobtype + '_'+ year +'.csv.gz'
-            response = requests.get(url,stream = True)
-            target_path =state + '_wac_S000_' + jobtype + '_'+ year +'.csv.gz'
-            with open(target_path,'wb') as f:
-                f.write(response.raw.read())
+        for state in states:
+        
+            for jobtype in types:
+        
+                url = 'https://lehd.ces.census.gov/data/lodes/LODES7/'+ state +'/wac/'+ state + '_wac_S000_' + jobtype + '_'+ year +'.csv.gz'
+                response = requests.get(url,stream = True)
+                target_path =state + '_wac_S000_' + jobtype + '_'+ year +'.csv.gz'
+                with open(target_path,'wb') as f:
+                    f.write(response.raw.read())
            
             # with tarfile.open(url,mode ='r') as f:
             #      f.extract(r'C:/CS-5010-Group_Project/' + state)
 
 # Loop through each tarfile and extract to CSV
-for file in os.listdir():
-    if file.endswith('.gz'):
-        file_s = file[:-3]
-        with gzip.open(file,'rt') as f_in:
-            with open(file_s,'wt') as f_out:
-              shutil.copyfileobj(f_in, f_out)
+    for file in os.listdir():
+        if file.endswith('.gz'):
+            file_s = file[:-3]
+            with gzip.open(file,'rt') as f_in:
+                with open(file_s,'wt') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
 
 # Read in each created CSV to a list, add year and employment type to each df
-data_list = []
-for file in os.listdir():
-    if file.endswith('.csv'):
-       year = file[:-4]
-       emp = file[:-9]
-       year = file[len(year) -4:len(year)]
-       emp = emp[len(emp) -4:len(emp)]
-       data = pd.read_csv(file)
-       data['Year'] = year
-       data['Emp_Type'] = emp
-       data_list.append(data)
+    data_list = []
+    for file in os.listdir():
+        if file.endswith('.csv'):
+            year = file[:-4]
+            emp = file[:-9]
+            year = file[len(year) -4:len(year)]
+            emp = emp[len(emp) -4:len(emp)]
+            data = pd.read_csv(file)
+            data['Year'] = year
+            data['Emp_Type'] = emp
+            data_list.append(data)
 
-df = pd.concat(data_list)
+    df = pd.concat(data_list)
 
+except Exception as e:
+    print('Error downloading: ', e)
+    
 # =============================================================================
 # Clean Data 
 # =============================================================================
--- Scripts/unit_tests.py
+++ Scripts/unit_tests.py
@@ -9,17 +9,40 @@ import os
 from LEHD_Script import *
 
 
+# =============================================================================
+# Check if all 45 files are read in from the Census
+# =============================================================================
+
+class test_scraped_files(unittest.TestCase):
+
+    def test_num_correct(self): 
+        files = [f for f in os.listdir() if f.endswith('.csv.gz')] # Get all downloaded files into object
+        self.assertEqual(len(files),45) # Assert that there are 45 tar files.
+
+
+
+if __name__ == '__main__':
+    unittest.main()    
+
+
+import unittest
+import os
+from LEHD_Script import *
+
 
 # =============================================================================
-# Check if all 47 files are read in 
+# Make sure all GEOIDs are 11 digits
 # =============================================================================
 
+class test_geoids(unittest.TestCase):
+    
+    def test_geoid_correct(self):
+        ids = df_tract['GEOID'].astype('str')
+        id_list = list(map(len,ids))
+        ret = [x for x in id_list if x == 11]
+        self.assertTrue(len(id_list),len(ret))    
 
-files = [f for f in os.listdir() if f.endswith('.csv')]
+    
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    unittest.main()    
